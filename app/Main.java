54b0bb8d14042cf7650197e56ded2f124dfdfad8
notKamui <teillard.jimmy@outlook.fr>
2019-05-07 10:42:02 +0200

    first commit

---
+++ app/Main.java
@@ -0,0 +1,36 @@
+package app;
+
+import java.io.File;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            URL url = new File("src/view/main.fxml").toURI().toURL();
+            loader.setLocation(url);
+            System.out.println(loader.getLocation());
+            BorderPane root = new BorderPane();
+            root=loader.load();
+            Scene scene = new Scene(root);
+            primaryStage.setResizable(false);
+            primaryStage.setTitle("Ziel");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
