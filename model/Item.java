fed698c0b634b24ab6c51f0cc40ee4aa54b28c3a
Nicollins <nledoux33@laposte.net>
2019-05-14 11:33:24 +0200

    Nico : reworked the inventory (void item)

--- model/Item.java
+++ model/Item.java
@@ -4,9 +4,14 @@ package model;
 public class Item {
 
 	private String name;
+	private int id;
 	
-	public Item(String n) {
+	public Item(String n, int id) {
 		this.name = n;
+		this.id = id;
 	}
 	
+	public int getId() {
+		return id;
+	}
 }
