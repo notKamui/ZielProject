f79705fa0a55fb7cf427f04ec6dfbc602a8249fe
notKamui <teillard.jimmy@outlook.fr>
2019-05-16 10:42:44 +0200

    notKamui : added GameObject class (extended by Player)

---
+++ model/GameObject.java
@@ -0,0 +1,42 @@
+package model;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class GameObject {
+    final private int GRAVITY = 2;
+    private IntegerProperty coordXProperty;
+    private IntegerProperty coordYProperty;
+    private Bounds hitbox;
+
+    public GameObject(int x, int y, int width, int height) {
+        this.coordXProperty = new SimpleIntegerProperty(x);
+        this.coordYProperty = new SimpleIntegerProperty(y);
+        this.hitbox = new Bounds(x, y, width, height);
+    }
+
+    public void move(int vectX, int vectY) {
+        this.setCoordXProperty(this.coordXProperty.get() + vectX);
+        this.setCoordYProperty(this.coordYProperty.get() + vectY);
+    }
+
+    public void gravity(){
+        this.move(0, GRAVITY);
+    }
+
+    public void setCoordXProperty(int x) {
+        this.coordXProperty.setValue(x);
+    }
+
+    public void setCoordYProperty(int y) {
+        this.coordYProperty.setValue(y);
+    }
+
+    public final IntegerProperty coordXProperty() {
+        return this.coordXProperty;
+    }
+
+    public final IntegerProperty coordYProperty() {
+        return this.coordYProperty;
+    }
+}
--- model/Player.java
+++ model/Player.java
@@ -1,41 +1,7 @@
 package model;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-
-public class Player extends Bounds {
-	final private int GRAVITY = 2;
-    private IntegerProperty coordXProperty;
-    private IntegerProperty coordYProperty;
-
+public class Player extends GameObject {
     public Player(int x, int y) {
     	super(x, y, 80, 80);
-        this.coordXProperty = new SimpleIntegerProperty(x);
-        this.coordYProperty = new SimpleIntegerProperty(y);
     }
-
-    public void move(int vectX, int vectY) {
-        this.setCoordXProperty(this.coordXProperty.get() + vectX);
-        this.setCoordYProperty(this.coordYProperty.get() + vectY);
-    }
-
-    public void gravity(){
-    	this.move(0, GRAVITY);
-    }
-    public void setCoordXProperty(int x) {
-        this.coordXProperty.setValue(x);
-    }
-
-    public void setCoordYProperty(int y) {
-        this.coordYProperty.setValue(y);
-    }
-
-    public final IntegerProperty coordXProperty() {
-        return this.coordXProperty;
-    }
-
-    public final IntegerProperty coordYProperty() {
-        return this.coordYProperty;
-    }
-    
 }
